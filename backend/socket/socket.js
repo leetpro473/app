d77b2d863c7b341c76caa27349cfd1b00796810f
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-10 10:23:23 -0300

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "https://aschopaa.com.br:3000",
+		origin: "http://aschopaa.com.br:3000",
 		methods: ["GET", "POST"],
 	},
 });
