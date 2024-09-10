394d85f5251513a63fdd2a69bc94be8aca9ff0f4
AsChopaaa <80022664+leetpro473@users.noreply.github.com>
2024-09-10 11:16:46 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -45,4 +45,4 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-server.listen(PORT, () => console.log(`Server started at http://reliquiaappbr.vercel.app:${PORT}`));
+server.listen(PORT, () => console.log(`Server started at http://aschopaa.com.br:${PORT}`));
